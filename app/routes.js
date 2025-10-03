02e67ad5152e6077a437166d4347669fd77e39f5
shawuxdesign <shawuxdesign@gmail.com>
2025-10-03 09:23:44 +0100

    Initial commit of space travel prototype

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,35 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.get('/start', function (req, res) {
+  res.render('start')
+})
+
+// Start page
+router.get('/start', function (req, res) {
+  res.render('start')
+})
+
+// Destination page
+router.get('/destination', function (req, res) {
+  res.render('destination')
+})
+
+router.post('/destination', function (req, res) {
+  res.redirect('/personal-details')
+})
+
+// Personal details page
+router.get('/personal-details', function (req, res) {
+  res.render('personal-details')
+})
+
+router.post('/personal-details', function (req, res) {
+  res.redirect('/check-answers')
+})
+
+// Check answers page
+router.get('/check-answers', function (req, res) {
+  res.render('check-answers')
+})
